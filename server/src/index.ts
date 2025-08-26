9db2ec3794ee7f9eb5d3c25db741f041214a408b
gurau-anil <gurau.anil@gmail.com>
2025-08-26 12:28:43 -0700

    typescript setup

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors'
+
+const app = express();
+const port = 5000;
+
+const corsOptions = {
+    origin: 'http://localhost:5174',
+    credentials: true
+  }
+app.use(cors(corsOptions));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express + TypeScript!');
+  });
+
+app.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+})
